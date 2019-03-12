ddebe552e355e51eef45b220b0fa6bf66f2a562b
Joel Salonen <SalonenJoppe@gmail.com>
2019-03-13 00:14:53 +0200

    fourth commit

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -31,6 +33,16 @@ public class BookstoreApplication {
 		return (args) -> {
 			Book book = new Book(category, "Testi", "Testi", "123456789", 25.00, 2010);
 			repository.save(book);
+		};
+	}
+	
+	@Bean
+	public CommandLineRunner demo2(UserRepository repository) {
+		return (args) -> {
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			repository.save(user1);
+			repository.save(user2);
 		};
 	}
 
